63da952862fca839b2d4de8a4d84139738a7d266
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2020-11-04 11:16:21 +0530

    Final UI and firebase Auth

--- app/src/main/java/com/rohan/hackathonapp/MainActivity.kt
+++ app/src/main/java/com/rohan/hackathonapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.rohan.hackathonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+
     }
+
 }
